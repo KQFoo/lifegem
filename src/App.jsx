b8ff3d6acaf2e667394cc8989c3637bfb4601c80
KQ <foongquan03@gmail.com>
2025-01-08 15:34:53 +0800

    update 2nd page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import background from './assets/cat2.jpg'
+import Profile from './components/Profile'
 
 function App() {
   const [name, setName] = useState("");
@@ -76,7 +77,7 @@ function App() {
     } else if (dataToSave.companyName !== "" || dataToSave.industry !== "" || dataToSave.netProfit !== 0 || dataToSave.stocks !== 0 || dataToSave.property !== 0 || dataToSave.crypto !== 0 || dataToSave.cash !== 0 || dataToSave.careerTitle !== "" || dataToSave.company !== "" || dataToSave.monthlyNetIncome !== 0) {
       localStorage.setItem("data", JSON.stringify(dataToSave));
       setSaving(true);
-      showNotification("Data saved successfully!", "success");
+      //showNotification("Data saved successfully!", "success");
     }
   }
 
@@ -87,109 +88,113 @@ function App() {
   return (
     <>
       <div className={`flex flex-col h-screen items-center justify-center relative ${popUp ? 'blur-none brightness-50' : ''}`} style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-        {submitted ? (
-          // <h1>Target Net Worth</h1>
-          <div className="w-1/2 border border-gray-300 rounded-lg shadow bg-white border-gray-300">
-            <ul className="hidden text-lg font-semibold text-center text-gray-700 divide-x divide-gray-300 rounded-lg sm:flex dark:divide-gray-300 dark:text-gray-700 rtl:divide-x-reverse" id="fullWidthTab" data-tabs-toggle="#fullWidthTabContent" role="tablist">
-              <li className="w-1/2">
-                <button onClick={() => handleTabClick("business")} type="button" role="tab" aria-controls="business" aria-selected={activeTab === "business"} className={`inline-block w-full p-4 rounded-ss-lg ${activeTab === "business" ? 'bg-[#FEF9C3]' : 'bg-white'} hover:bg-[#FEF9C3] focus:outline-none`}>Business</button>
-              </li>
-              <li className="w-1/2">
-                <button onClick={() => handleTabClick("investment")} type="button" role="tab" aria-controls="investment" aria-selected={activeTab === "investment"} className={`inline-block w-full p-4 ${activeTab === "investment" ? 'bg-[#FEF9C3]' : 'bg-white'} hover:bg-[#FEF9C3] focus:outline-none`}>Investment</button>
-              </li>
-              <li className="w-1/2">
-                <button onClick={() => handleTabClick("job")} type="button" role="tab" aria-controls="job" aria-selected={activeTab === "job"} className={`inline-block w-full p-4 rounded-se-lg ${activeTab === "job" ? 'bg-[#FEF9C3]' : 'bg-white'} hover:bg-[#FEF9C3] focus:outline-none`}>Job</button>
-              </li>
-            </ul>
-            <div id="fullWidthTabContent" className="border-t border-gray-300">
-              {/* Business */}
-              <div className={`p-4 bg-white rounded-lg md:p-8 ${activeTab === "business" ? '' : 'hidden'}`} id="business" role="tabpanel" aria-labelledby="business-tab">
-                <div className="mb-4">
-                  <label className="block mb-2 text-sm font-medium text-gray-900">Target Company Name</label>
-                  <input type="text" id="companyName" placeholder="Lifegem" className="block w-full p-2 text-gray-900 border border-gray-300 bg-gray-50" />
-                </div>
-                <div className="mb-4">
-                  <label className="block mb-2 text-sm font-medium text-gray-900">Target Industry</label>
-                  <input type="text" id="industry" placeholder="Software" className="block w-full p-2 text-gray-900 border border-gray-300 bg-gray-50" />
-                </div>
-                <div className="">
-                  <label className="block mb-2 text-sm font-medium text-gray-900">Target Monthly Net Profit (USD)</label>
-                  <input type="number" min={0} id="netProfit" placeholder="100,000" className="block w-full p-2 text-gray-900 border border-gray-300 bg-gray-50" />
-                </div>
-                {/* <div className="flex flex-col items-center">
+        {saving ? (<Profile />) : (
+          submitted ? (
+            // <h1>Target Net Worth</h1>
+            <>
+              <h1 className="mb-4 text-white font-light text-3xl">Welcome, {name}!</h1>
+              <div className="w-1/2 border border-gray-300 rounded-lg shadow bg-white border-gray-300">
+                <ul className="hidden text-lg font-semibold text-center text-gray-700 divide-x divide-gray-300 rounded-lg sm:flex dark:divide-gray-300 dark:text-gray-700 rtl:divide-x-reverse" id="fullWidthTab" data-tabs-toggle="#fullWidthTabContent" role="tablist">
+                  <li className="w-1/2">
+                    <button onClick={() => handleTabClick("business")} type="button" role="tab" aria-controls="business" aria-selected={activeTab === "business"} className={`inline-block w-full p-4 rounded-ss-lg ${activeTab === "business" ? 'bg-[#FEF9C3]' : 'bg-white'} hover:bg-[#FEF9C3] focus:outline-none`}>Business</button>
+                  </li>
+                  <li className="w-1/2">
+                    <button onClick={() => handleTabClick("investment")} type="button" role="tab" aria-controls="investment" aria-selected={activeTab === "investment"} className={`inline-block w-full p-4 ${activeTab === "investment" ? 'bg-[#FEF9C3]' : 'bg-white'} hover:bg-[#FEF9C3] focus:outline-none`}>Investment</button>
+                  </li>
+                  <li className="w-1/2">
+                    <button onClick={() => handleTabClick("job")} type="button" role="tab" aria-controls="job" aria-selected={activeTab === "job"} className={`inline-block w-full p-4 rounded-se-lg ${activeTab === "job" ? 'bg-[#FEF9C3]' : 'bg-white'} hover:bg-[#FEF9C3] focus:outline-none`}>Job</button>
+                  </li>
+                </ul>
+                <div id="fullWidthTabContent" className="border-t border-gray-300">
+                  {/* Business */}
+                  <div className={`p-4 bg-white rounded-lg md:p-8 ${activeTab === "business" ? '' : 'hidden'}`} id="business" role="tabpanel" aria-labelledby="business-tab">
+                    <div className="mb-4">
+                      <label className="block mb-2 text-sm font-medium text-gray-900">Target Company Name</label>
+                      <input type="text" id="companyName" placeholder="Lifegem" className="block w-full p-2 text-gray-900 border border-gray-300 bg-gray-50" />
+                    </div>
+                    <div className="mb-4">
+                      <label className="block mb-2 text-sm font-medium text-gray-900">Target Industry</label>
+                      <input type="text" id="industry" placeholder="Software" className="block w-full p-2 text-gray-900 border border-gray-300 bg-gray-50" />
+                    </div>
+                    <div className="">
+                      <label className="block mb-2 text-sm font-medium text-gray-900">Target Monthly Net Profit (USD)</label>
+                      <input type="number" min={0} id="netProfit" placeholder="100,000" className="block w-full p-2 text-gray-900 border border-gray-300 bg-gray-50" />
+                    </div>
+                    {/* <div className="flex flex-col items-center">
                   <button onClick={""} type="submit" className="mt-4 rounded-full w-48 px-4 py-1 border text-lg text-gray-900 bg-gray-200 transition duration-300 hover:bg-gray-300"><strong>Save Changes</strong></button>
                 </div> */}
-              </div>
-              <div className={`p-4 bg-white rounded-lg md:p-8 ${activeTab === "investment" ? '' : 'hidden'}`} id="investment" role="tabpanel" aria-labelledby="investment-tab">
-                {/* Investment */}
-                <div className="mb-4">
-                  <label className="block mb-2 text-sm font-medium text-gray-900">Stocks/ETF (USD)</label>
-                  <input type="number" min={0} id="stocks" placeholder="4,000,000" className="block w-full p-2 text-gray-900 border border-gray-300 bg-gray-50" />
-                </div>
-                <div className="mb-4">
-                  <label className="block mb-2 text-sm font-medium text-gray-900">Property/Land (USD)</label>
-                  <input type="number" min={0} id="property" placeholder="3,000,000" className="block w-full p-2 text-gray-900 border border-gray-300 bg-gray-50" />
-                </div>
-                <div className="mb-4">
-                  <label className="block mb-2 text-sm font-medium text-gray-900">Crypto (USD)</label>
-                  <input type="number" min={0} id="crypto" placeholder="2,000,000" className="block w-full p-2 text-gray-900 border border-gray-300 bg-gray-50" />
-                </div>
-                <div className="">
-                  <label className="block mb-2 text-sm font-medium text-gray-900">Cash (USD)</label>
-                  <input type="number" min={0} id="cash" placeholder="1,000,000" className="block w-full p-2 text-gray-900 border border-gray-300 bg-gray-50" />
-                </div>
-                {/* <div className="flex flex-col items-center">
+                  </div>
+                  <div className={`p-4 bg-white rounded-lg md:p-8 ${activeTab === "investment" ? '' : 'hidden'}`} id="investment" role="tabpanel" aria-labelledby="investment-tab">
+                    {/* Investment */}
+                    <div className="mb-4">
+                      <label className="block mb-2 text-sm font-medium text-gray-900">Stocks/ETF (USD)</label>
+                      <input type="number" min={0} id="stocks" placeholder="4,000,000" className="block w-full p-2 text-gray-900 border border-gray-300 bg-gray-50" />
+                    </div>
+                    <div className="mb-4">
+                      <label className="block mb-2 text-sm font-medium text-gray-900">Property/Land (USD)</label>
+                      <input type="number" min={0} id="property" placeholder="3,000,000" className="block w-full p-2 text-gray-900 border border-gray-300 bg-gray-50" />
+                    </div>
+                    <div className="mb-4">
+                      <label className="block mb-2 text-sm font-medium text-gray-900">Crypto (USD)</label>
+                      <input type="number" min={0} id="crypto" placeholder="2,000,000" className="block w-full p-2 text-gray-900 border border-gray-300 bg-gray-50" />
+                    </div>
+                    <div className="">
+                      <label className="block mb-2 text-sm font-medium text-gray-900">Cash (USD)</label>
+                      <input type="number" min={0} id="cash" placeholder="1,000,000" className="block w-full p-2 text-gray-900 border border-gray-300 bg-gray-50" />
+                    </div>
+                    {/* <div className="flex flex-col items-center">
                   <button onClick={""} type="submit" className="mt-4 rounded-full w-48 px-4 py-1 border text-lg text-gray-900 bg-gray-200 transition duration-300 hover:bg-gray-300"><strong>Save Changes</strong></button>
                 </div> */}
-              </div>
-              <div className={`p-4 bg-white rounded-lg md:p-8 ${activeTab === "job" ? '' : 'hidden'}`} id="job" role="tabpanel" aria-labelledby="job-tab">
-                {/* Job */}
-                <div className="mb-4">
-                  <label className="block mb-2 text-sm font-medium text-gray-900">Target Career Title</label>
-                  <input type="text" id="careerTitle" placeholder="Principal Engineer" className="block w-full p-2 text-gray-900 border border-gray-300 bg-gray-50" />
-                </div>
-                <div className="mb-4">
-                  <label className="block mb-2 text-sm font-medium text-gray-900">Target Company Name</label>
-                  <input type="text" id="company" placeholder="Google" className="block w-full p-2 text-gray-900 border border-gray-300 bg-gray-50" />
-                </div>
-                <div className="">
-                  <label className="block mb-2 text-sm font-medium text-gray-900">Target Monthly Net Income (USD)</label>
-                  <input type="number" min={0} id="monthlyNetIncome" placeholder="100,000" className="block w-full p-2 text-gray-900 border border-gray-300 bg-gray-50" />
-                </div>
-                {/* <div className="flex flex-col items-center">
+                  </div>
+                  <div className={`p-4 bg-white rounded-lg md:p-8 ${activeTab === "job" ? '' : 'hidden'}`} id="job" role="tabpanel" aria-labelledby="job-tab">
+                    {/* Job */}
+                    <div className="mb-4">
+                      <label className="block mb-2 text-sm font-medium text-gray-900">Target Career Title</label>
+                      <input type="text" id="careerTitle" placeholder="Principal Engineer" className="block w-full p-2 text-gray-900 border border-gray-300 bg-gray-50" />
+                    </div>
+                    <div className="mb-4">
+                      <label className="block mb-2 text-sm font-medium text-gray-900">Target Company Name</label>
+                      <input type="text" id="company" placeholder="Google" className="block w-full p-2 text-gray-900 border border-gray-300 bg-gray-50" />
+                    </div>
+                    <div className="">
+                      <label className="block mb-2 text-sm font-medium text-gray-900">Target Monthly Net Income (USD)</label>
+                      <input type="number" min={0} id="monthlyNetIncome" placeholder="100,000" className="block w-full p-2 text-gray-900 border border-gray-300 bg-gray-50" />
+                    </div>
+                    {/* <div className="flex flex-col items-center">
                   <button onClick={""} type="submit" className="mt-4 rounded-full w-48 px-4 py-1 border text-lg text-gray-900 bg-gray-200 transition duration-300 hover:bg-gray-300"><strong>Save Changes</strong></button>
                 </div> */}
+                  </div>
+                  <div className="flex flex-row mb-6 justify-center items-center">
+                    <label className="text-sm font-medium text-gray-900">Target Year:</label>
+                    <input type="number" id="targetYear" min={new Date().getFullYear()} max={new Date().getFullYear() + 100} placeholder={new Date().getFullYear()} className="ml-3 block w-1/5 p-1 text-gray-900 border border-gray-300 bg-gray-50 text-xs" />
+                  </div>
+                  <div className="flex flex-row justify-center items-center">
+                    <button onClick={() => setPopUp(true)} type="submit" className="mb-6 rounded-full w-40 px-4 py-1 border text-lg text-gray-900 bg-gray-200 transition duration-300 hover:bg-gray-300"><strong>Back</strong></button>
+                    <button onClick={handleSave} type="submit" className="ml-4 mb-6 rounded-full w-48 px-4 py-1 border text-lg text-gray-900 bg-[#FEF9C3] transition duration-300 hover:bg-[#FEF08A]"><strong>Save Changes</strong></button>
+                  </div>
+                </div>
               </div>
-              <div className="flex flex-row mb-6 justify-center items-center">
-                <label className="text-sm font-medium text-gray-900">Target Year:</label>
-                <input type="number" id="targetYear" min={new Date().getFullYear()} max={new Date().getFullYear() + 100} placeholder={new Date().getFullYear()} className="ml-3 block w-1/5 p-1 text-gray-900 border border-gray-300 bg-gray-50 text-xs" />
+            </>
+          ) : (
+            <>
+              <h1 className="text-8xl font-semibold text-white hover:text-[#FEF9C3]">
+                <span className="hover:underline">Your Life, Your Gem.</span>
+              </h1>
+              <h3 className="text-3xl font-semibold mt-2.5 mb-4 text-white hover:text-[#FEF9C3]">
+                <span className="hover:underline">Harness the power of visualization to fuel your motivation.</span>
+              </h3>
+              <div className="flex p-8">
+                <span className="inline-flex items-center px-4 text-lg text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 border-e-0 rounded-s-md transition duration-300 hover:bg-gray-300">
+                  <svg className="w-6 h-6 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                    <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+                  </svg>
+                </span>
+                <input onChange={e => setName(e.target.value)} type="text" id="name" className="rounded-none bg-gray-50 border text-gray-900 block flex-1 min-w-0 w-full text-lg p-3 transition duration-300 transform hover:scale-110 focus:border-black focus:outline" placeholder="your name"
+                />
+                <button onClick={handleNext} type="submit" className="inline-flex items-center px-4 text-lg text-gray-900 bg-gray-200 border rounded-s-0 border-gray-300 border-s-0 rounded-e-md transition duration-300 hover:bg-gray-300"><strong>Next</strong></button>
               </div>
-              <div className="flex flex-row justify-center items-center">
-                <button onClick={() => setPopUp(true)} type="submit" className="mb-6 rounded-full w-40 px-4 py-1 border text-lg text-gray-900 bg-gray-200 transition duration-300 hover:bg-gray-300"><strong>Back</strong></button>
-                <button onClick={handleSave} type="submit" className="ml-4 mb-6 rounded-full w-48 px-4 py-1 border text-lg text-gray-900 bg-[#FEF9C3] transition duration-300 hover:bg-[#FEF08A]"><strong>Save Changes</strong></button>
-              </div>
-            </div>
-          </div>
-        ) : (
-          <>
-            <h1 className="text-8xl font-semibold text-white hover:text-[#FEF9C3]">
-              <span className="hover:underline">Your Life, Your Gem.</span>
-            </h1>
-            <h3 className="text-3xl font-semibold mt-2.5 mb-4 text-white hover:text-[#FEF9C3]">
-              <span className="hover:underline">Visualize your success vision to get motivated.</span>
-            </h3>
-            <div className="flex p-8">
-              <span className="inline-flex items-center px-4 text-lg text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 border-e-0 rounded-s-md transition duration-300 hover:bg-gray-300">
-                <svg className="w-6 h-6 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                  <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
-                </svg>
-              </span>
-              <input onChange={e => setName(e.target.value)} type="text" id="name" className="rounded-none bg-gray-50 border text-gray-900 block flex-1 min-w-0 w-full text-lg p-3 transition duration-300 transform hover:scale-110 focus:border-black focus:outline" placeholder="your name"
-              />
-              <button onClick={handleNext} type="submit" className="inline-flex items-center px-4 text-lg text-gray-900 bg-gray-200 border rounded-s-0 border-gray-300 border-s-0 rounded-e-md transition duration-300 hover:bg-gray-300"><strong>Next</strong></button>
-            </div>
-          </>
-        )}
+            </>
+          ))}
       </div>
       {popUp ? (
         <div id="info-popup" tabindex="-1" className="flex items-center justify-center fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
